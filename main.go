f75f323ae2a0a226f1b8c6be61a333bf81ff233f
Andres Villagra <andres.villagra@alumnos.unc.edu.ar>
2019-05-14 10:06:31 -0300

    Moved MoodEntry to it's own file

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"fmt"
 	"os"
 	"os/user"
@@ -33,37 +32,6 @@ func CreateSaveDirectory() error {
 	moodFile := filepath.Join(dataDirectory, "moods.csv")
 	file, err := os.Create(moodFile)
 	file.Close()
-	return err
-}
-
-type MoodEntry struct {
-	Rating uint64
-	Date   time.Time
-}
-
-func (m MoodEntry) String() string {
-	return fmt.Sprintf("%s: %d/10",
-		m.Date.Format("Mon 2/1/2006"),
-		m.Rating)
-}
-
-func (m MoodEntry) CSVFormat() []string {
-	return []string{m.Date.String(), fmt.Sprintf("%d", m.Rating)}
-}
-
-func (m MoodEntry) Save() error {
-	moodFile := filepath.Join(dataDirectory, "moods.csv")
-	file, err := os.OpenFile(moodFile,
-		os.O_RDWR|os.O_APPEND|os.O_CREATE,
-		0755)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-	records := m.CSVFormat()
-	err = writer.Write(records)
 	return err
 }
 
---
+++ mood-entry.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type MoodEntry struct {
+	Rating uint64
+	Date   time.Time
+}
+
+func (m MoodEntry) String() string {
+	return fmt.Sprintf("%s: %d/10",
+		m.Date.Format("Mon 2/1/2006"),
+		m.Rating)
+}
+
+func (m MoodEntry) CSVFormat() []string {
+	return []string{m.Date.String(), fmt.Sprintf("%d", m.Rating)}
+}
+
+func (m MoodEntry) Save() error {
+	moodFile := filepath.Join(dataDirectory, "moods.csv")
+	file, err := os.OpenFile(moodFile,
+		os.O_RDWR|os.O_APPEND|os.O_CREATE,
+		0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	records := m.CSVFormat()
+	err = writer.Write(records)
+	return err
+}
