37bd80ffc4d9e05cecc5ab8ab72280e3a938f80c
Andres Villagra <andres.villagra@alumnos.unc.edu.ar>
2019-05-15 10:08:49 -0300

    Added record parsing

--- mood-entry.go
+++ mood-entry.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
+
+const dateFormat string = "2 1 2006"
+
+var BadRecord error = errors.New("Bad record.")
 
 type MoodEntry struct {
 	Rating uint64
 	Date   time.Time
 }
 
+func FromCSVRecord(record []string) (*MoodEntry, error) {
+	if len(record) != 2 {
+		return nil, BadRecord
+	}
+	time, err := time.Parse(record[0], dateFormat)
+	if err != nil {
+		return nil, err
+	}
+	rating, err := strconv.ParseUint(record[1], 10, 64)
+	entry := &MoodEntry{
+		Rating: rating,
+		Date:   time,
+	}
+	return entry, nil
+}
+
 func (m MoodEntry) String() string {
 	return fmt.Sprintf("%s: %d/10",
-		m.Date.Format("Mon 2/1/2006"),
+		m.Date.Format(dateFormat),
 		m.Rating)
 }
 
 func (m MoodEntry) CSVFormat() []string {
-	return []string{m.Date.String(), fmt.Sprintf("%d", m.Rating)}
+	return []string{m.Date.Format(dateFormat), fmt.Sprintf("%d", m.Rating)}
 }
 
 func (m MoodEntry) Save(moodFile string) error {
